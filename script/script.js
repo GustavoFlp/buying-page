35907978917fed79cf973eaef5ce18f6ba8521b6
gardu <gf9068586@gmail.com>
2022-01-13 19:35:57 -0300

    3.0

---
+++ script/script.js
@@ -0,0 +1,5 @@
+var button = document.getElementById('button')
+var compra = document.getElementById('compra')
+button = addEventListener('click', () =>{
+    window.scroll({top: innerHeight * 6})
+})
